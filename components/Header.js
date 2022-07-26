ad79af6a0736dcc70dcd68a3a941de12cb6c76f9
Pavel <braurs64@gmail.com>
2022-07-26 11:11:35 +0400

    fix text & del DD

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-import Chevron from '../public/chevron.svg'
 import Logo from '../public/logo.svg'
 
 function Header() {
@@ -17,30 +16,13 @@ function Header() {
       </div>
       <div className="flex">
         <Link href="/">
-          <a className="btn-yellow">About</a>
+          <a className="btn-yellow">Get started</a>
         </Link>
-        <div className="flex gap-2 btn-yellow group">
-          <div className="relative">
-            <a className="cursor-pointer">Projects</a>
-            <div className="dropdown">
-              <Link href="/">
-                <a className="btn-yellow">Apps</a>
-              </Link>
-              <Link href="/">
-                <a className="btn-yellow">Components</a>
-              </Link>
-            </div>
-          </div>
-          <Image
-            src={Chevron}
-            alt=""
-            width="6"
-            height="4"
-            className="group-hover:rotate-180"
-          />
-        </div>
         <Link href="/">
-          <a className="btn-yellow">Get Involved</a>
+          <a className="btn-yellow">Learning</a>
+        </Link>
+        <Link href="/">
+          <a className="btn-yellow">Explore</a>
         </Link>
       </div>
     </div>
