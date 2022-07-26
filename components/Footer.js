52e59e857171f44c96254b504ba17d2fda80376a
Pavel <braurs64@gmail.com>
2022-07-26 11:04:08 +0400

    fix footer (del dropdown)

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-import Chevron from '../public/chevron-white.svg'
 import Logo from '../public/logo-grey.svg'
 
 function Footer() {
@@ -18,30 +17,13 @@ function Footer() {
       <div className="">2022 © unfoldingWord</div>
       <div className="flex ">
         <Link href="/">
-          <a className="btn-transparent">About</a>
+          <a className="btn-transparent">Get started</a>
         </Link>
-        <div className="flex btn-transparent group">
-          <div className="relative">
-            <a className="cursor-pointer btn-transparent">Projects</a>
-            <div className="dropdown -top-32">
-              <Link href="/">
-                <a className="btn-yellow">Apps</a>
-              </Link>
-              <Link href="/">
-                <a className="btn-yellow">Components</a>
-              </Link>
-            </div>
-          </div>
-          <Image
-            src={Chevron}
-            alt=""
-            width="6"
-            height="4"
-            className="group-hover:rotate-180"
-          />
-        </div>
         <Link href="/">
-          <a className="btn-transparent">Get Involved</a>
+          <a className="btn-transparent">Learning</a>
+        </Link>
+        <Link href="/">
+          <a className="btn-transparent">Explore</a>
         </Link>
       </div>
     </div>
