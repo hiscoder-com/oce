a4a104b789a956aeaaf454a518c81987f5035250
Pavel <braurs64@gmail.com>
2022-07-26 11:16:40 +0400

    wip

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
+import Chevron from '../public/chevron.svg'
 import Logo from '../public/logo.svg'
 
 function Header() {
@@ -18,11 +19,28 @@ function Header() {
         <Link href="/">
           <a className="btn-yellow">Get started</a>
         </Link>
+        <div className="flex gap-2 btn-yellow group">
+          <div className="relative">
+            <a className="cursor-pointer">Learning</a>
+            <div className="dropdown">
+              <Link href="/">
+                <a className="btn-yellow">Apps</a>
+              </Link>
+              <Link href="/">
+                <a className="btn-yellow">Components</a>
+              </Link>
+            </div>
+          </div>
+          <Image
+            src={Chevron}
+            alt=""
+            width="6"
+            height="4"
+            className="group-hover:rotate-180"
+          />
+        </div>
         <Link href="/">
-          <a className="btn-yellow">Learning</a>
-        </Link>
-        <Link href="/">
-          <a className="btn-yellow">Explore</a>
+          <a className="btn-yellow">Get Involved</a>
         </Link>
       </div>
     </div>
