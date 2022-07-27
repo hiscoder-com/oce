1c2ca0f7cb9c14ebd245d9542ce072c19fe4f03d
Vitek Foxprogs <foxprogs@gmail.com>
2022-07-27 12:58:59 +0400

    lint hooks folder

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
   eslint: {
-    dirs: ['pages', 'utils', 'components'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    dirs: ['pages', 'utils', 'components', 'hooks'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
 }
 
