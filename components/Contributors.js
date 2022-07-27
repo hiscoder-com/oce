998dc96eb9cab946af52b7dc0e4251fc31055a90
Pavel <braurs64@gmail.com>
2022-07-27 06:43:11 +0400

    added array contributors

---
+++ components/Contributors.js
@@ -0,0 +1,27 @@
+function Contributors() {
+  const contributors = [
+    {
+      login: 'foxprogs',
+      avatar_url: 'https://avatars.githubusercontent.com/u/30548361?v=4',
+      html_url: 'https://github.com/foxprogs',
+    },
+    {
+      login: 'Valyukhov',
+      avatar_url: 'https://avatars.githubusercontent.com/u/74174349?v=4',
+      html_url: 'https://github.com/Valyukhov',
+    },
+    {
+      login: 'Vorobushec',
+      avatar_url: 'https://avatars.githubusercontent.com/u/68908261?v=4',
+      html_url: 'https://github.com/Vorobushec',
+    },
+    {
+      login: 'Pavel064',
+      avatar_url: 'https://avatars.githubusercontent.com/u/60795829?v=4',
+      html_url: 'https://github.com/Pavel064',
+    },
+  ]
+  return <div>Contributors</div>
+}
+
+export default Contributors
