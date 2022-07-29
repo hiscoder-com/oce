bc8a0e92b6097a0c96acbc9aff7f539f0f490939
Vorobusec <timur.yancev08@gmail.com>
2022-07-29 16:57:42 +0400

    WIP

--- components/HeaderStaticPage.js
+++ components/HeaderStaticPage.js
@@ -6,17 +6,24 @@ function HeaderStaticPage({ config }) {
   return (
     <div className=" border-b-2 border-dashed my-16  pr-10">
       <Link href="/">
-        <button className="flex justify-between w-28">
+        <button className="flex justify-between w-28 hover:text-gray-700 text-gray-500">
           <div className="w-5 h-3">
-            <Image
-              className="back-svg "
-              width="100%"
-              height="100%"
-              alt="back"
-              src={'/back.svg'}
-            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className=""
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+              />
+            </svg>
           </div>
-          <p className="text-gray-500 text-base font-semibold">Get started</p>
+          <p className=" text-base font-semibold">Get started</p>
         </button>
       </Link>
       <div className="flex justify-between items-center">
