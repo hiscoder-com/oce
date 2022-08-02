66c1c7dac9fc4da2d7db85d8e9cf1dcb40020dfc
Pavel <braurs64@gmail.com>
2022-08-02 09:18:03 +0400

    fix footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,33 +1,61 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
+import { useRouter } from 'next/router'
+
 import Logo from '../public/logo-white.svg'
+import discord_black from '../public/discord_black.svg'
 
 function Footer() {
+  const router = useRouter()
+
   return (
-    <footer className="bg-black mt-6">
+    <footer className="bg-primary-600 mt-6">
       <div className="footer container mx-auto px-2">
-        <Link href="/">
-          <a className="flex items-center gap-5 pl-2">
-            <Image src={Logo} alt="Open Components Ecosystem" width="34" height="47" />
-            Open Components
-          </a>
-        </Link>
-        <div className="text-text-200">2022 © unfoldingWord</div>
-        <div>
-          <Link href="/about">
-            <a className="btn-transparent">About</a>
-          </Link>
-          <Link href="/components">
-            <a className="btn-transparent">Explore</a>
-          </Link>
-          <Link href="/get-started">
-            <a className="btn-transparent">Get started</a>
-          </Link>
+        <div className="flex items-center gap-9">
           <Link href="/">
-            <a className="btn-transparent">FAQ</a>
+            <a className="flex gap-5 pl-2">
+              <Image src={Logo} alt="Open Components Ecosystem" width="34" height="36" />
+            </a>
           </Link>
+
+          <button
+            onClick={() => {
+              router.push(`https://discord.com/invite/auJb4H9ezx`)
+            }}
+            className="w-44 h-11 rounded-lg text-white bg-primary-600"
+          >
+            <div className="flex justify-center items-center w-full h-full gap-2 rounded-lg bg-primary-100 text-black hover:bg-secondary-600 active:shadow-xl active:shadow-primary-700/23">
+              <div className="flex justify-center">
+                <Image
+                  src={discord_black}
+                  alt="discord_black"
+                  width="24"
+                  height="23"
+                  className=""
+                />
+              </div>
+              <div>Join Discord</div>
+            </div>
+          </button>
+
+          <div>
+            <Link href="/about">
+              <a className="btn-transparent">About</a>
+            </Link>
+            <Link href="/components">
+              <a className="btn-transparent">Explore</a>
+            </Link>
+            <Link href="/get-started">
+              <a className="btn-transparent">Get started</a>
+            </Link>
+            <Link href="/">
+              <a className="btn-transparent">FAQ</a>
+            </Link>
+          </div>
         </div>
+
+        <div className="cursor-default text-text-200">2022 © unfoldingWord</div>
       </div>
     </footer>
   )
--- pages/index.js
+++ pages/index.js
@@ -107,9 +107,11 @@ export default function Home() {
               <div className="text-base text-justify text-text-600 group-hover:text-black">
                 Find out what the Open Component Ecosystem (OCE) is all about. To learn
                 about the concept behind the OCE, you can check out our{' '}
-                <span className="font-bold underline decoration-2 underline-offset-4">
-                  whitepaper
-                </span>{' '}
+                <Link href="/about">
+                  <a className="font-bold underline decoration-2 underline-offset-4">
+                    whitepaper
+                  </a>
+                </Link>{' '}
                 here.
               </div>
             </div>
