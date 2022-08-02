3514040c6a4e2c2137a253386bd972b6c1acac54
Pavel <braurs64@gmail.com>
2022-08-02 10:54:09 +0400

    del import

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
-import { shouldInclude } from '@apollo/client/utilities'
 import ComponentCard from '../components/ComponentCard'
 import AppCard from '../components/AppCard'
 import { apps, components } from '../utils/helper'
