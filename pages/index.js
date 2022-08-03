a529ae23d6281a46ec482db75d47f078b743b348
Pavel <braurs64@gmail.com>
2022-08-03 11:20:49 +0400

    fix arrow-svg

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,6 @@ import { apps, components } from '../utils/helper'
 
 import editor_black from '../public/editor_black.svg'
 import ascent from '../public/ascent.svg'
-import right from '../public/right.svg'
-import discord from '../public/discord.svg'
 import introduction from '../public/introduction.svg'
 import explore from '../public/explore.svg'
 import discord_hero from '../public/discord_hero.svg'
@@ -46,32 +44,25 @@ export default function Home() {
             </div>
 
             <div className="flex flex-col gap-5">
-              <button
-                onClick={() => {
-                  router.push(`/`)
-                }}
-                className="flex flex-row gap-10 items-center text-primary-600 group-hover:text-black"
-              >
-                <Link href="/get-started">
-                  <a className="text-2xl font-bold underline decoration-primary-600 decoration-2 underline-offset-4 max-w-max group-hover:decoration-black">
-                    Get started
-                  </a>
-                </Link>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 mt-2 cursor-pointer"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                  />
-                </svg>
-              </button>
+              <Link href="/get-started">
+                <a className="flex flex-row gap-10 items-center text-2xl font-bold text-primary-600 underline decoration-primary-600 decoration-2 underline-offset-4 max-w-max group-hover:decoration-black group-hover:text-black">
+                  Get started
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 mt-1"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    />
+                  </svg>
+                </a>
+              </Link>
               <div className="text-base text-justify text-text-600 group-hover:text-black">
                 Get started with the Open Component Ecosystem. The community conversation
                 is ongoing on Discord, but here you can find out more about how to begin
@@ -85,13 +76,23 @@ export default function Home() {
                 target="_blank"
                 className="flex justify-center items-center w-44 h-11 gap-2 rounded-lg hover:bg-primary-700 active:shadow-xl active:shadow-primary-700/23"
               >
-                <div className="flex justify-center">
-                  <Image src={discord} alt="discord" width="24" height="23" />
+                <div className="flex justify-center text-white">
+                  <svg
+                    role="img"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Discord</title>
+                    <path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z" />
+                  </svg>
                 </div>
                 <div>Join Discord</div>
               </a>
             </Link>
-          </div>{' '}
+          </div>
         </div>
 
         <div className="flex flex-col pt-16 pb-8 px-9 justify-between gap-5 w-1/3 bg-primary-100 group rounded-lg hover:bg-secondary-400">
@@ -100,32 +101,25 @@ export default function Home() {
               <Image src={introduction} alt="introduction" width="303" height="185" />
             </div>
             <div className="flex flex-col gap-5">
-              <button
-                onClick={() => {
-                  router.push(`/`)
-                }}
-                className="flex flex-row gap-10 items-center max-w-max text-primary-600 group-hover:text-black"
-              >
-                <Link href="/about">
-                  <a className="text-2xl font-bold underline decoration-primary-600 decoration-2 underline-offset-4 group-hover:decoration-black">
-                    Introduction
-                  </a>
-                </Link>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 mt-2 cursor-pointer"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                  />
-                </svg>
-              </button>
+              <Link href="/about">
+                <a className="flex flex-row gap-10 items-center max-w-max text-2xl font-bold underline text-primary-600 decoration-primary-600 decoration-2 underline-offset-4 group-hover:text-black group-hover:decoration-black">
+                  Introduction
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 mt-1"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    />
+                  </svg>
+                </a>
+              </Link>
               <div className="text-base text-justify text-text-600 group-hover:text-black">
                 Find out what the Open Component Ecosystem (OCE) is all about. To learn
                 about the concept behind the OCE, you can check out our{' '}
@@ -157,32 +151,25 @@ export default function Home() {
               <Image src={explore} alt="explore" width="303" height="185" />
             </div>
             <div className="flex flex-col gap-5">
-              <button
-                onClick={() => {
-                  router.push(`/`)
-                }}
-                className="inline-flex gap-10 items-center max-w-max text-primary-600 group-hover:text-black"
-              >
-                <Link href="/components">
-                  <a className="text-2xl font-bold underline decoration-primary-600 decoration-2 underline-offset-4 group-hover:decoration-black">
-                    Explore
-                  </a>
-                </Link>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 mt-2 cursor-pointer"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                  />
-                </svg>
-              </button>
+              <Link href="/components">
+                <a className="flex items-center gap-10 max-w-max text-2xl font-bold text-primary-600 underline decoration-2 underline-offset-4 decoration-primary-600 group-hover:text-black group-hover:decoration-black">
+                  Explore
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 mt-1"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    />
+                  </svg>
+                </a>
+              </Link>
               <div className="text-base text-justify text-text-600 group-hover:text-black">
                 Discover the various components that are already available in the
                 ecosystem and the related apps using them.
