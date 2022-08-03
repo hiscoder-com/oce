3928982cda34263413925a01d8a6418be5747a4d
valyukhov <alexzed@bk.ru>
2022-08-03 14:05:26 +0300

    fixes changes

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -3,52 +3,40 @@ import axios from 'axios'
 export default async function handler(req, res) {
   const {
     body: {
+      ref,
       commits,
-      repository: {
-        description,
-        full_name,
-        owner,
-        topics,
-        homepage,
-        language,
-        name,
-        master_branch,
-      },
+      repository: { description, full_name, topics, homepage, language, master_branch },
     },
   } = req
 
   const isChange = (file) => {
     return commits.some((el) => {
-      return el.added.includes(file) || el.modified.includes(file)
+      return (
+        el.added.includes(file) || el.modified.includes(file) || el.removed.includes(file)
+      )
     })
   }
   let data = {
-    repository: { description, full_name, owner: owner.name, topics, homepage, language },
+    repository: { description, full_name, topics, homepage, language },
   }
 
   if (!commits || !commits?.length > 0) {
     return res.status(200).json(data)
   }
+  if (master_branch !== ref.split('/')[2]) {
+    return res.status(200).json(data)
+  }
   if (isChange('oce.json')) {
     try {
       const result = await axios.get(
-        `https://raw.githubusercontent.com/${owner.name}/${name}/${master_branch}/oce.json`
+        `https://raw.githubusercontent.com/${full_name}/${master_branch}/oce.json`
       )
 
       data['oce'] = result.data
     } catch (error) {
-      console.log(error)
-    }
-  }
-  if (isChange('README.md')) {
-    try {
-      const result = await axios.get(
-        `https://raw.githubusercontent.com/${owner.name}/${name}/${master_branch}/README.md`
-      )
-
-      data['readme'] = result.data
-    } catch (error) {
-      console.log(error)
+      // data['oce'] = {}
+      res.status(404).json(error)
+      return
     }
   }
 
