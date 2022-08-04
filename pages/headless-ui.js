fb62f716cd19ed9b2879a7453cc91a43222aef74
Vorobusec <timur.yancev08@gmail.com>
2022-08-04 18:39:38 +0400

    changed font sizes of static pages

--- pages/headless-ui.js
+++ pages/headless-ui.js
@@ -25,7 +25,7 @@ export default function HeadlessUI() {
             src={'/headless-ui/pic1.jpg'}
             alt="lego island"
           />
-          <figcaption className="text-gray-400">
+          <figcaption className="mt-1 text-gray-400">
             Glen Carrie —{' '}
             <Link href="https://unsplash.com/photos/HpMihL323k0">
               <a target="_blank">
@@ -36,23 +36,24 @@ export default function HeadlessUI() {
         </figure>
         <br />
         <br />
-        <h2 className="font-bold text-2xl">Introduction</h2>
-        <p>
+        <h2 className="font-bold text-3xl">Introduction</h2>
+        <br />
+        <p className="text-xl leading-8">
           A headless user interface component is a component that offers maximum visual
           flexibility by providing no interface. This might sound like providing a user
           interface pattern without providing a user interface. That’s funny but true.
         </p>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           I will demonstrate this pattern with React but this pattern is not specific to
           React. It is just a pattern that helps to build reusable user interface
           components.
         </p>
         <br />
         <br />
-        <h2 className="font-bold text-2xl">What is it?</h2>
+        <h2 className="font-bold text-3xl">What is it?</h2>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           Headless components are the components that have no user interface but have
           functionality. It does not care about how your components look, how they are
           designed, styled. It just gives you the functionality and how it will look when
@@ -61,7 +62,7 @@ export default function HeadlessUI() {
         </p>
         <br />
         <blockquote className="border-black border-l-2 pl-3">
-          <p>
+          <p className="text-xl leading-8">
             <strong>
               <em>Rule of Separation:</em>
             </strong>{' '}
@@ -73,13 +74,13 @@ export default function HeadlessUI() {
         <br />
         <h2 className="font-bold text-2xl">What is the problem?</h2>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           Sometimes components are way too tied to the design they present. They are made
           to cater to the designs that can be found at the time of them being made but
           have no mindfulness of future enhancements.
         </p>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           Most of the time specifications and needs of a component change, by a product
           owner or a UX designer. Developers should have a mindset that a component might
           change in the future and changing the component should not take so much of their
@@ -87,7 +88,7 @@ export default function HeadlessUI() {
           will be harsh and will be open to create new bugs.
         </p>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           Headless components cover all of these concerns by having nothing to do with the
           user interface and just providing functionality. When you provide stable,
           well-tested functionality, changing a component’s design will be a lot easier
@@ -95,9 +96,9 @@ export default function HeadlessUI() {
         </p>
         <br />
         <br />
-        <h2 className="font-bold text-2xl">Let’s get into an example!</h2>
+        <h2 className="font-bold text-3xl">Let’s get into an example!</h2>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           The code right below is an example of a Theme component. It is just for a
           demonstration and it is not a headless component. It just sets the current theme
           of an application and displays what the current theme is.
@@ -108,7 +109,7 @@ export default function HeadlessUI() {
           <p>theme component, not headless</p>
         </div>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           As you can see here, the user interface and logic are coupled here. Let’s say
           that you want to change <b>Set as Light</b> and <b>Set as Dark</b> texts to the
           icon. You need to pass them as props and you need to add a couple of props to
@@ -118,14 +119,14 @@ export default function HeadlessUI() {
         <Gist id="41ea88e412160460a8f82bff265d6b49" />
         <p className="description-gist">theme component with icon props, not headless</p>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           In the code snippet, we have added two new props, <b>lightIcon</b> and{' '}
           <b>darkIcon</b>. We have changed our Theme component to handle these props. If
           they are present, icons will be shown. Otherwise, old behavior will still work
           and texts will be displayed.
         </p>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           Let’s say that it is not enough and we have a new silly requirement which is:
           current theme should not be shown on some pages. That means it should be
           controlled by the parent component. With a new prop, <b>showCurrentTheme</b>.
@@ -136,18 +137,20 @@ export default function HeadlessUI() {
           theme component with showCurrentTheme prop, not headless
         </p>
         <br />
-        <h2 className="font-bold text-2xl">Yes, we have done it!</h2>
+        <h2 className="font-bold text-3xl">Yes, we have done it!</h2>
         <br />
-        <p>At the end let’s summarize what this simple silly Theme component does:</p>
+        <p className="text-xl leading-8">
+          At the end let’s summarize what this simple silly Theme component does:
+        </p>
         <br />
-        <ul className="list-disc ml-6">
+        <ul className="list-disc ml-6 text-xl leading-8">
           <li className="mb-2">It displays the current theme status</li>
           <li className="mb-2">It allows you to change the theme as light</li>
           <li className="mb-2">It allows you to change the theme as dark</li>
           <li className="mb-2">It allows you to hide the current theme status</li>
         </ul>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           With each requirement, our component will change, and let’s assume that there
           will be a couple of more new requirements to this component and it will be more
           complex. It is going to be hard to maintain and follow components by developers.
@@ -155,16 +158,16 @@ export default function HeadlessUI() {
           affecting an old behavior.
         </p>
         <br />
-        <h2 className="font-bold text-2xl">Headless component will rescue!</h2>
-        <p>
-          <br />
+        <h2 className="font-bold text-3xl">Headless component will rescue!</h2>
+        <br />
+        <p className="text-xl leading-8">
           The possibilities with headless component implementation will be almost endless.
           You can do whatever you want with your user interface by having what is provided
           with a headless theme component. Let’s see how to write this component with
           react custom hook.
         </p>
         <br />
-        <p className="italic">
+        <p className="italic text-xl leading-8">
           I should mention that before react hooks headless components are generally
           implemented with render props pattern or a higher-order component. You can
           search these patterns and understand how to implement headless components with
@@ -176,7 +179,7 @@ export default function HeadlessUI() {
           headless component implemented with react custom hook
         </p>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           As you can see there is nothing related to the user interface. We have
           implemented a headless component with react custom hook and provided
           functionalities to set theme, <b>setDark</b>, <b>setLight</b>, and also provided
@@ -189,7 +192,7 @@ export default function HeadlessUI() {
           some examples that use theme headless component
         </p>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           These are some basic examples to give you an idea about how to use headless
           components and what they are exactly. I know it is a simple, dummy example but
           it covers what headless components are all about. This article is not about how
@@ -199,9 +202,11 @@ export default function HeadlessUI() {
         </p>
         <br />
         <br />
-        <p>There are great libraries that implement headless components. Like:</p>
+        <p className="text-xl leading-8">
+          There are great libraries that implement headless components. Like:
+        </p>
         <br />
-        <ul className="list-disc ml-6 font-bold underline underline-offset-2">
+        <ul className="list-disc ml-6 font-bold underline underline-offset-2 text-xl leading-8">
           <Link href="https://tanstack.com/table/v8/?from=reactTableV7&original=https://react-table-v7.tanstack.com/">
             <a target="_blank">
               <li>react-table</li>
@@ -219,21 +224,23 @@ export default function HeadlessUI() {
           </Link>
         </ul>
         <br />
-        <p>
+        <p className="text-xl leading-8">
           I suggest you take a look around the source code of these libraries, especially
           react-table.
         </p>
         <br />
         <br />
-        If it is not enough for you, there are lots of sources about headless components.
-        Take a look at{' '}
-        <Link href="https://www.merrickchristensen.com/articles/headless-user-interface-components/">
-          <a target="_blank">
-            <u>those articles</u>
-          </a>
-        </Link>
-        . I would suggest you read this article which is written by{' '}
-        <b>Merrick Christensen.</b>
+        <p className="text-xl leading-8 mb-16">
+          If it is not enough for you, there are lots of sources about headless
+          components. Take a look at{' '}
+          <Link href="https://www.merrickchristensen.com/articles/headless-user-interface-components/">
+            <a target="_blank">
+              <u>those articles</u>
+            </a>
+          </Link>
+          . I would suggest you read this article which is written by{' '}
+          <b>Merrick Christensen.</b>
+        </p>
       </div>
     </div>
   )
--- pages/react-components-library.js
+++ pages/react-components-library.js
@@ -296,7 +296,7 @@ export default function ReactCompLibrary() {
           Open <code className="bg-gray-100">http://localhost:6060</code> and you should
           see the following:
         </p>
-        <div className="w-full h-auto">
+        <div className="w-9/12 h-auto m-auto">
           <Image
             layout="responsive"
             width="692"
@@ -366,8 +366,9 @@ export default function ReactCompLibrary() {
         </p>
         <br />
         <p className="text-xl leading-8">
-          Now, open <code className="bg-gray-100">http://localhost:6060</code>again and it
-          should include the Button Component in the docs as shown in the picture below.
+          Now, open <code className="bg-gray-100">http://localhost:6060</code> again and
+          it should include the Button Component in the docs as shown in the picture
+          below.
         </p>
         <br />
         <div className="w-full h-auto">
