c48419e7c1a768e7a4a54baa84bad0d1f2bb1f04
Pavel <braurs64@gmail.com>
2022-08-09 14:23:00 +0400

    added indents for blocks

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
   const router = useRouter()
 
   return (
-    <div className="flex flex-col gap-4 mx-4 md:gap-10">
+    <div className="flex flex-col gap-10 mx-4 xl:gap-12">
       <Head>
         <title>Open Components Ecosystem</title>
         <meta name="description" content="Open Components Ecosystem" />
@@ -183,11 +183,11 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col gap-2">
-        <div className="flex flex-col gap-2">
+      <div className="flex flex-col md:gap-10 xl:gap-0">
+        <div className="flex flex-col gap-10 xl:gap-12">
           <div className="uppercase text-text-500">Components</div>
           <div className="flex flex-col lg:flex-row lg:gap-5 xl:gap-12 2xl:gap-0">
-            <div className="grid my-1 gap-5 sm:grid-cols-2 sm:gap-3 md:grid-cols-3 md:my-2 lg:grid-cols-4 xl:my-8 xl:gap-5 2xl:w-9/12 2xl:gap-8">
+            <div className="grid gap-5 sm:grid-cols-2 sm:gap-3 md:grid-cols-3 lg:grid-cols-4 xl:gap-5 2xl:w-9/12 2xl:gap-8">
               {components.map((el) => ComponentCard({ repo: el }))}
               <Link href="/components">
                 <a className="hidden items-center gap-2.5 mx-auto my-24 font-bold underline text-primary-600 decoration-primary-600 decoration-2 underline-offset-4 md:flex lg:hidden">
@@ -231,10 +231,10 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-10 xl:gap-12">
           <div className="uppercase text-text-500">Applications</div>
           <div className="flex flex-col lg:flex-row lg:gap-5 xl:gap-12 2xl:gap-0">
-            <div className="grid gap-5 my-1 sm:grid-cols-1 sm:gap-3 md:grid-cols-2 md:my-2 md:gap-3 xl:my-8 xl:gap-5 2xl:w-9/12 2xl:gap-8">
+            <div className="grid gap-5 sm:grid-cols-1 sm:gap-3 md:grid-cols-2 md:gap-3 xl:gap-5 2xl:w-9/12 2xl:gap-8">
               {apps?.slice(0, 2)?.map((el) => AppCard({ repo: el }))}
             </div>
             <Link href="/apps">
