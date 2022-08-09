7f6e61b5d90f090e8c18cf68c2183353cf70152b
Pavel <braurs64@gmail.com>
2022-08-09 23:11:11 +0400

    fix close-icon in header

--- components/Header.js
+++ components/Header.js
@@ -38,13 +38,13 @@ function Header() {
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          className="cursor-pointer md:hidden"
+          className="cursor-pointer md:hidden text-primary-600"
         >
           <path
             fillRule="evenodd"
             clipRule="evenodd"
             d="M3 18H21V16H3V18ZM3 13H21V11H3V13ZM3 6V8H21V6H3Z"
-            fill="#1D1D1D"
+            fill="currentColor"
           />
         </svg>
       </div>
