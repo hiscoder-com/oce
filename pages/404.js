500dde59635e3be85b6adbd6bcc1ea6c872b028a
Vorobusec <timur.yancev08@gmail.com>
2022-08-15 11:49:50 +0400

    fix

--- pages/404.js
+++ pages/404.js
@@ -3,9 +3,11 @@ import Link from 'next/link'
 const NotFound = () => {
   return (
     <div className="not-found container">
-      <div className="max-w-full height-work-area flex items-center justify-center">
-        <div className="text-7xl border-r-2 border-black pr-4">404</div>
-        <div className="ml-4 text-2xl">
+      <div className="max-w-full height-work-area flex items-center justify-center mx-8">
+        <div className="text-4xl border-r-2 border-black pr-2 md:text-7xl md:pr-4">
+          404
+        </div>
+        <div className="ml-2 text-base md:text-2xl md:ml-4">
           <p>Ooops...</p>
           <p>That page cannot be found :(</p>
         </div>
