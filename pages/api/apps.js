0ff777bbdccbaf1bfcbc8948c92623e3b562188c
Vitek Foxprogs <foxprogs@gmail.com>
2022-08-18 23:11:26 +0400

    added logo for apps

--- pages/api/apps.js
+++ pages/api/apps.js
@@ -1,5 +1,7 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
 
+import { prisma } from '../../utils/prisma'
+
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   cache: new InMemoryCache(),
@@ -55,7 +57,34 @@ export default async function handler(req, res) {
         }
       `,
     })
-    res.status(200).json(result.data.search)
+
+    const appsArr = await prisma.repo.findMany({
+      take: 100,
+      orderBy: {
+        createdAt: 'desc',
+      },
+      where: {
+        topics: {
+          some: {
+            topic: {
+              name: 'scripture-open-apps',
+            },
+          },
+        },
+      },
+      select: {
+        repo: true,
+        logo: true,
+      },
+    })
+    let apps = {}
+    if (appsArr.length > 0) {
+      for (let app of appsArr) {
+        apps[app.repo] = app.logo
+      }
+    }
+
+    res.status(200).json({ git: result.data.search, db: apps })
   } catch (error) {
     res.status(404).json(error)
   }
